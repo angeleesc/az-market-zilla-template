d2141b7889b07594d8c6f637246a7f0eb0940da3
Salas <hugo.salas@nokia.com>
2024-02-05 12:11:59 -0500

    configuracion inicial de las rutas

---
+++ app/(marketplace)/page.jsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h3>hola angel XD dentro del grupo</h3>
+    </main>
+  );
+}
