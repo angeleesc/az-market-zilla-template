fa07461c5f392cd929814a49e8283fcc85a920cb
Salas <hugo.salas@nokia.com>
2024-02-06 15:39:54 -0500

    configuracion del hero swiper

--- components/swipers/hero-swiper/HeroSwiper.jsx
+++ components/swipers/hero-swiper/HeroSwiper.jsx
@@ -1,21 +1,44 @@
-"use client"
+
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
+import HeroSwiperSlide from './HeroSwiperSlide';
+
+const tempImgSwiper = [
+
+    {
+        img: "https://res.cloudinary.com/angelplayxd/image/upload/v1707251471/este-sera-precio-samsung-galaxy-s24-seran-mas-baratos-esperado_98_du0dl7.webp",
+        message: "Ve los nuevos telefonos de samsung",
+
+    },
+
+    {
+        img: "https://res.cloudinary.com/angelplayxd/image/upload/v1707251471/Apple-WWCD23-Vision-Pro-EyeSight-230605.jpg.large_2x_i0ey2p.jpg",
+        message: "Disponibles las apple vision pro",
+
+    },
+
+]
+
 
 export default function HeroSwiper() {
-  return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
-  )
+    return (
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+
+            {
+                tempImgSwiper.map((img, i) => {
+                    return <SwiperSlide key={`hero-image-${i}`}>
+                        {`ejemplo ${i}`}
+                    </SwiperSlide>
+
+                })
+            }
+
+        </Swiper>
+    )
 }
---
+++ components/swipers/hero-swiper/HeroSwiperSlide.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { SwiperSlide } from 'swiper/react'
+
+export default function HeroSwiperSlide({image, description, productLink }) {
+  return (
+   <div className='ejemplo'>
+   <h3>tjemplo dento de un swiper</h3>
+   </div>
+  )
+}
