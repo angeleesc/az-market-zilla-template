40d42181273d907db481a64cb3123c212403e562
Salas <hugo.salas@nokia.com>
2024-02-05 21:16:50 -0500

    middle bar

--- components/middleBar/MiddleBar.jsx
+++ components/middleBar/MiddleBar.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import React from 'react'
 import { Col, Container, Row } from 'react-bootstrap'
 import SearchBar from '../search-bar/SearchBar'
+import {FaRegHeart, FaCartShopping} from "react-icons/fa6"
 
 export default function MiddleBar() {
     return (
@@ -20,9 +21,29 @@ export default function MiddleBar() {
                     <Col md={7}className='text-center' >
                         <SearchBar/>
                     </Col>
-                    <Col lg={3}>
-                        <div className='text-right'>
-                            <ul className='list-none p-0 m-0' ></ul>
+                    <Col lg={3} className='flex items-center justify-end'>
+                        <div className='text-right '>
+                            <ul className='list-none p-0 m-0 inline-flex' >
+
+                                <li className='mx-[10px] font-bold relative text-[18px]' >
+                                    <Link href={"/"} className='no-underline text-gray-500' >
+                                        <FaRegHeart />
+                                        <span className='absolute top-[-15px] right-[-15px] bg-theme-primary text-[12px] p-[2px] rounded-full text-gray-50' >9+</span>
+                                    </Link>
+                                </li>
+                                <li className='mx-[10px] font-bold relative text-[18px]' >
+                                    <Link href={"/"} className='no-underline text-gray-500' >
+                                        <FaCartShopping />
+                                        <span className='absolute top-[-15px] right-[-15px] bg-theme-primary text-[12px] p-[2px] rounded-full text-gray-50' >9+</span>
+                                    </Link>
+                                </li>
+                                <li className='mx-[10px] font-bold relative text-[18px]' >
+                                    <Link href={"/"} className='no-underline text-gray-500' >
+                                    155.65$
+                                    </Link>
+                                </li>
+
+                            </ul>
 
                         </div>
                     </Col>
