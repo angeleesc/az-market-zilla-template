ee75c4a45cf6cf9ec9b28584fb7b49e405f67408
Salas <hugo.salas@nokia.com>
2024-02-18 17:35:02 -0500

    configuracion de modulos de ventas de swiper

---
+++ app/(marketplace)/product-details/[id]/page.jsx
@@ -0,0 +1,10 @@
+import ProductDetails from '@/components/templates/ProductDetails'
+import React from 'react'
+
+export default function page() {
+  return (
+    <>
+     <ProductDetails/>
+    </>
+  )
+}
---
+++ components/templates/ProductDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ProductDetailSwiper from '../swipers/product-details-swiper/ProductDetailSwiper'
+
+export default function ProductDetails() {
+  return (
+   <>
+    <div className='flex flex-wrap my-3 px-[60px]'>
+        <ProductDetailSwiper/>
+        </div>
+   </>
+  )
+}
