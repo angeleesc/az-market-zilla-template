c3cac8cd1c3f0da733bef0f447755ae91570a5fa
Salas <hugo.salas@nokia.com>
2024-02-18 15:02:05 -0500

    maqutacion del footer

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -6,14 +6,12 @@ export default function Footer() {
         <footer className={`
         px-[60px] py-[20px]
          bg-theme-darck
-          flex flex-wrap 
-          justify-center 
-          md:justify-start
+          flex fl
           [&_ul]:p-0
             [&_a]:text-gray-400
                 [&_a]:no-underline
-                [&_a]:hover:text-theme-primary
                 [&_a]:text-14px
+                [&_a]:font-light
                 [&_a]:transition-colors
                 [&_p]:text-gray-500
                 [&_p]:text-[14px]
@@ -23,28 +21,60 @@ export default function Footer() {
                 [&_6]:text-white
                 [&_h6]:m-0
         `}>
-            <div className='max-w-[300px] min-w-[200px] grow text-white '>
-                <h3 class="text-white font-sans text-[24px] font-bold">Angel <span class="text-theme-primary">Zilla</span></h3>
-                <ul>
-                    <li>
-                        <Link href={"/"}>
-                            inicio
-                        </Link>
-                    </li>
-                    <li>
-                        <h6>Direcion</h6>
-                        <p>Direccionde la tienda</p>
-                    </li>
-                </ul>
-            </div>
-            <div className='max-w-[300px] min-w-[200px] grow text-white '>
-                coluna 2
-            </div>
-            <div className='max-w-[300px] min-w-[200px] grow text-white '>
-                coluna 3
-            </div>
-            <div className='max-w-[300px] min-w-[200px] grow text-white '>
-                coluna 4
+
+            <div className='w-full flex flex-wrap justify-center md:justify-start' >
+                <h3 class="text-white font-sans text-[24px] font-bold w-full">Angel <span class="text-theme-primary">Zilla</span></h3>
+                <div className='max-w-[300px] min-w-[200px] grow text-white p-3'>
+                    <ul>
+                        <li>
+                            <Link href={"/"} className='hover:text-theme-primary'>
+                                inicio
+                            </Link>
+                        </li>
+                        <li>
+                            <h6>telefono</h6>
+                            <p>+58 414 378 7747</p>
+                        </li>
+                        <li>
+                            <h6>Direcion</h6>
+                            <p>av urdaneta edificion banesco piso 1, oficina 6, Caracas Venezuela</p>
+                        </li>
+                        <li>
+                            <h6>Email</h6>
+                            <p>angel@angelzilla.com</p>
+                        </li>
+                    </ul>
+                </div>
+                <div className='max-w-[300px] min-w-[200px] grow text-white p-3'>
+                    <ul>
+                        <li>
+                            <h6 className='relative '>
+                                Atajos
+                            </h6>
+                        </li>
+                        <li>
+                            <Link href={"/"} className='hover:text-theme-primary'>
+                                Soperote tecnico
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={"/"} className='hover:text-theme-primary'>
+                                Acreca
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href={"/"} className='hover:text-theme-primary'>
+                                Electrodomesticos
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className='max-w-[300px] min-w-[200px] grow text-white p-3'>
+                    coluna 3
+                </div>
+                <div className='max-w-[300px] min-w-[200px] grow text-white p-3'>
+                    coluna 4
+                </div>
             </div>
         </footer>
     )
