dc03ccfb1b7f13f25512c9ab35a5f6303e2d2d84
Salas <hugo.salas@nokia.com>
2024-02-18 14:22:38 -0500

    configuracion del tema base

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,7 +1,51 @@
+import Link from 'next/link'
 import React from 'react'
 
 export default function Footer() {
-  return (
-    <footer  className='px-[60px] py-[20px] bg-theme-darck'>Footer</footer>
-  )
+    return (
+        <footer className={`
+        px-[60px] py-[20px]
+         bg-theme-darck
+          flex flex-wrap 
+          justify-center 
+          md:justify-start
+          [&_ul]:p-0
+            [&_a]:text-gray-400
+                [&_a]:no-underline
+                [&_a]:hover:text-theme-primary
+                [&_a]:text-14px
+                [&_a]:transition-colors
+                [&_p]:text-gray-500
+                [&_p]:text-[14px]
+                [&_li]:my-2
+                [&_h6]:text-14px
+                [&_h6]:font-medium
+                [&_6]:text-white
+                [&_h6]:m-0
+        `}>
+            <div className='max-w-[300px] min-w-[200px] grow text-white '>
+                <h3 class="text-white font-sans text-[24px] font-bold">Angel <span class="text-theme-primary">Zilla</span></h3>
+                <ul>
+                    <li>
+                        <Link href={"/"}>
+                            inicio
+                        </Link>
+                    </li>
+                    <li>
+                        <h6>Direcion</h6>
+                        <p>Direccionde la tienda</p>
+                    </li>
+                </ul>
+            </div>
+            <div className='max-w-[300px] min-w-[200px] grow text-white '>
+                coluna 2
+            </div>
+            <div className='max-w-[300px] min-w-[200px] grow text-white '>
+                coluna 3
+            </div>
+            <div className='max-w-[300px] min-w-[200px] grow text-white '>
+                coluna 4
+            </div>
+        </footer>
+    )
 }
