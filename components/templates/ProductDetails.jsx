e3f200252b65b7b3fb0e4add3802d109bf93afc1
Salas <hugo.salas@nokia.com>
2024-02-18 22:42:59 -0500

    moduloa de prueba de her shwper

--- components/templates/ProductDetails.jsx
+++ components/templates/ProductDetails.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ProductDetailSwiper from '../swipers/product-details-swiper/ProductDetailSwiper'
 
+const images = [
+    "/images/product-details-1.jpg",
+    "/images/product-details-2.jpg",
+    "/images/product-details-3.jpg",
+    "/images/product-details-4.jpg",
+    "/images/product-details-5.jpg",
+    "/images/product-details-6.jpg",
+    "/images/product-details-7.jpg",
+    "/images/product-details-8.jpg",
+    "/images/product-details-9.jpg",
+    ]
+
 export default function ProductDetails() {
   return (
    <>
     <div className='flex flex-wrap my-3 px-[60px]'>
-        <ProductDetailSwiper/>
+        <ProductDetailSwiper images = {images} productId={"galaxy-tab"}/>
         </div>
    </>
   )
