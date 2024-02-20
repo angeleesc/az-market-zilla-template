abb783d617791659b631670d7bcc3e30d9e1f34b
Salas <hugo.salas@nokia.com>
2024-02-20 15:12:38 -0500

    configuracion de un swiper

--- components/swipers/product-details-swiper/ProductDetailSwiper.jsx
+++ components/swipers/product-details-swiper/ProductDetailSwiper.jsx
@@ -1,8 +1,8 @@
 "use-client"
-import React from 'react'
+import React, { useState } from 'react'
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa"
-import { EffectFade, Navigation, Pagination, A11y } from 'swiper/modules';
+import { EffectFade, Navigation, Pagination, A11y, Thumbs } from 'swiper/modules';
 
 import 'swiper/css';
 import 'swiper/css/effect-fade';
@@ -13,26 +13,31 @@ import SwiperProductButton from './SwiperProductButton';
 
 export default function ProductDetailSwiper({ images, productId }) {
 
+    const [activeThumbs, setActiveThumbs] = useState()
+
 
     return (
-        <div className='max-w-[600px] flex justify-center h-[600px] md:h-[600px]'>
-            <div className='w-full'>
+        <div className='max-w-[600px] min-h-[600px]'>
+       
                 <Swiper
                     spaceBetween={50}
                     slidesPerView={1}
                     onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => console.log(swiper)}
-                    modules={[Navigation, A11y, Pagination]}
+                    onSwiper={(swiper) => console.log("swiper clix")}
+                    modules={[Navigation, A11y, Pagination, Thumbs]}
                     loop
                     // navigation={true}
                     className='az-product-details-swiper'
+                    thumbs={{
+                         swiper: activeThumbs
+                    }}
 
                 >
                     {
                         images.map((img, i) => {
-                            return <SwiperSlide key={`${productId}-${i}`} className='w-full  h-full flex justify-center'>
-                                <div className='w-full h-[600px] flex justify-center items-center'>
-                                    <img src={img} alt={`${productId}-${i}`} />
+                            return <SwiperSlide key={`${productId}-${i}`} className='w-full  h-full flex justify-center '>
+                                <div className='w-full h-[600px] flex justify-center items-center rounded'>
+                                    <img src={img} alt={`${productId}-${i}`} className='rounded-[16px]' />
                                 </div>
                             </SwiperSlide>
 
@@ -40,8 +45,34 @@ export default function ProductDetailSwiper({ images, productId }) {
                     }
                     <SwiperProductButton />
                 </Swiper>
+                <Swiper
+                    spaceBetween={10}
+                    slidesPerView={5}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={setActiveThumbs}
 
-            </div>
+                    modules={[Navigation, A11y, Pagination, Thumbs]}
+                    loop
+                    // navigation={true}
+                    className='az-product-details-swiper-thumnails'
+
+                >
+                    {
+                        images.map((img, i) => {
+                            return <SwiperSlide key={`${productId}-${i}`} className=''>
+                                <div className='w-full h-[60px]  flex justify-center items-center  p-1 hover:cursor-pointer hover:opacity-80'>
+                                    <img src={img} alt={`${productId}-${i}`} className=' h-full'  />
+                                </div>
+                            </SwiperSlide>
+
+                        })
+                    }
+                </Swiper>
+
+                
+                
+
+            
 
 
 
