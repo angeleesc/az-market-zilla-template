69bbc897d2972b5f0a29a5655dee1b7a6d3fe41d
Salas <hugo.salas@nokia.com>
2024-02-20 19:31:27 -0500

    maquetacion de precios

--- components/counters/shop-counters/ShopCounters.jsx
+++ components/counters/shop-counters/ShopCounters.jsx
@@ -1,12 +1,52 @@
 import React, { useState } from 'react'
+import {FaMinus, FaPlus} from "react-icons/fa6"
 
 export default function ShopCounters({ }) {
     const [count, setCount] = useState(1)
     const [readMode, setReadMode] = useState(true)
+
+    const addCount = () => setCount(Number(count + 1))
+    const subtractionCount = () => {
+        if (count <= 1) {
+            setCount(1)
+        } else {
+            setCount(count - 1)
+        }
+    }
+
     return (
-        <div className='flex'>
-            <button type='button' onClick={()=>setCount(Number(count+1))}>angregar</button>
+        <div className='flex h-[40px] items-stretch'>
+            <button type='button'
+                className={`
+                w-[50px]
+                h-full
+                transition-colors
+                border
+                border-gray-500
+                flex
+                justify-center
+                items-center
+                hover:bg-theme-primary
+                hover:text-white
+                rounded-s-[8px]
+                `}
+
+                onClick={addCount}>
+                    <FaPlus/>
+                </button>
             <input type="text"
+                className={
+                    `
+                px-3
+                py-1
+                max-w-[100px]
+                outline-none
+                border
+                border-gray-500
+                h-full  
+                `
+
+                }
                 readOnly={readMode}
                 value={count}
 
@@ -15,18 +55,19 @@ export default function ShopCounters({ }) {
                     setCount(Number(e.target.value))
                 }}
                 onBlur={() => {
-                    if(count < 1) setCount(1)
+                    if (count < 1) setCount(1)
                     setReadMode(true)
                 }}
             />
             <button type='button'
-            onClick={()=>{
-                if(count <= 1){
-                    setCount(1)
-                }else{
-                    setCount(count-1)
-                }
-            }}
+                className={`
+            w-[50px]
+            h-full
+            transition-colors
+            border
+            border-gray-500
+            `}
+                onClick={subtractionCount}
             >quitar</button>
         </div>
     )
