8b07ceb88ab9d615010bbd16890675b268ab82fc
Salas <hugo.salas@nokia.com>
2024-02-20 20:46:54 -0500

    maquetacion de la rewie

--- components/tabs/info-detail-tab/InfoDetailsTab.jsx
+++ components/tabs/info-detail-tab/InfoDetailsTab.jsx
@@ -1,13 +1,19 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
+import InfoTabBar from './InfoTabBar'
+import "./info-detatils-tab-contianes.scss"
 
 
 
-export default function InfoDetailsTab({specifications, info, rewies}) {
-const [currentTab, setcurrentTab] = useState(second)
+export default function InfoDetailsTab({specifications, info, reviews}) {
+
+const [currentTab, setcurrentTab] = useState("specifications")
     
 
   return (
-    <div>InfoDetailsTab</div>
+    <div className='az-details-tabs-container'>
+        <InfoTabBar/>
+        
+    </div>
   )
 }
--- components/tabs/info-detail-tab/InfoTabBar.jsx
+++ components/tabs/info-detail-tab/InfoTabBar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
 
-export default function InfoTabBar() {
-  return (
-    <div>InfoTabBar</div>
-  )
+export default function InfoTabBar({ currentOption, updateOption }) {
+    return (
+        <nav className={
+            `flex
+        justify-center
+        items-stretch
+        h-[60px]
+        w-full
+        az-info-tab-bar`
+        }>
+            <ul className='flex p-0 m-0'>
+                <li><button>Especificaciones</button></li>
+                <li><button>Imformacion</button></li>
+                <li><button>Calificaciones</button></li>
+            </ul>
+        </nav>
+    )
 }
