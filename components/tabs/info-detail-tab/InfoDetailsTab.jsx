242b4bb717e378d3e4a693de49c031737940d997
Salas <hugo.salas@nokia.com>
2024-02-24 17:45:09 -0500

    agragando los compenentes

--- components/tabs/info-detail-tab/InfoDetailsTab.jsx
+++ components/tabs/info-detail-tab/InfoDetailsTab.jsx
@@ -2,21 +2,34 @@
 import React, { useState } from 'react'
 import InfoTabBar from './InfoTabBar'
 import "./info-detatils-tab-contianes.scss"
+import SpecificationsTab from './SpecificationsTab'
+import RewiesTabs from './RewiesTabs'
+import InfoTab from './InfoTab'
 
 
 
-export default function InfoDetailsTab({specifications, info, reviews}) {
+export default function InfoDetailsTab({ specifications, info, reviews }) {
 
-const [currentTab, setcurrentTab] = useState("specifications")
-    
+  const [currentTab, setcurrentTab] = useState("specifications")
+
+  specifications
+  info
+  reviews
+
 
   return (
     <div className='az-details-tabs-container'>
-        <InfoTabBar currentOption={currentTab} updateOption={setcurrentTab}/>
-        {
-          
-        }
-        
+      <InfoTabBar currentOption={currentTab} updateOption={setcurrentTab} />
+      {
+        currentTab == "specifications" && <SpecificationsTab />
+      }
+      {
+        currentTab == "info" && <InfoTab/>
+      }
+      {
+        currentTab == "reviews" && <RewiesTabs/>
+      }
+
     </div>
   )
 }
