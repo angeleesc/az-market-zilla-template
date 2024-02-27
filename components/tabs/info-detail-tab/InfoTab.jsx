a6ecf8b97e9e53d994c177587ba03cc10bf4e8b8
Salas <hugo.salas@nokia.com>
2024-02-27 12:09:57 -0500

    configuracion del ta de secciones

--- components/tabs/info-detail-tab/InfoTab.jsx
+++ components/tabs/info-detail-tab/InfoTab.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useRef } from 'react'
 
 export default function InfoTab({productId, data}) {
+
+  const boxRef = useRef()
+  useEffect(()=>{
+    if(boxRef.current){
+      boxRef.current.innerHTML = data
+    }
+
+  boxRef.current=null
+  })
+
+
   return (
-    <div>
-       {data}
+    <div ref={boxRef} className='info-section'> 
+    
     </div>
   )
 }
