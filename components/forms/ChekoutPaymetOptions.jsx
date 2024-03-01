349caafb3e07020bcb4c0b64c44a617472ba004f
Salas <hugo.salas@nokia.com>
2024-03-01 13:31:49 -0500

    activacion de la opcion de span

--- components/forms/ChekoutPaymetOptions.jsx
+++ components/forms/ChekoutPaymetOptions.jsx
@@ -2,20 +2,24 @@ import React from 'react'
 
 export default function ChekoutPaymetOptions({ register, current, option }) {
     return (
-        <div className='flex flex-wrap az-chekout-payment-option'>
+        <div className=' az-chekout-payment-option'>
             forma de pago
-            <label className=''>
-                <span>Tarjeta</span>
-                <input value={"card"}  type="radio" {...register} />
-            </label>
-            <label>
-                <span>Paypal</span>
-                <input value={"paypal"} type="radio" {...register} />
-            </label>
-            <label>
-                <span>Zinli</span>
-                <input value={"paypal"} type="radio" {...register} />
-            </label>
+            <div className='flex flex-wrap w-full'>
+                <label className='az-chekout-payment-radio'>
+                    <input value={"card"} type="radio" {...register} />
+                    <span>Tarjeta</span>
+                </label>
+                <label className='az-chekout-payment-radio'>
+                    <input value={"paypal"} type="radio" {...register} />
+                    <span>Paypal</span>
+                </label>
+                <label className='az-chekout-payment-radio'>
+                    <input value={"paypal"} type="radio" {...register} />
+                    <span>Zinli</span>
+                </label>
+
+            </div>
+
         </div>
     )
 }
