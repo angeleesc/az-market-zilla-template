6d6720f2d17db717060b51164bf1406480e6b7d1
Salas <hugo.salas@nokia.com>
2024-03-05 18:42:47 -0500

    configuracion de datos

--- components/forms/CheckoutForm.jsx
+++ components/forms/CheckoutForm.jsx
@@ -2,10 +2,16 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import "./form-zone.scss"
 import ChekoutPaymetOptions from './ChekoutPaymetOptions'
+import { FaCcVisa, FaCcMastercard, } from "react-icons/fa6"
+import { SiAmericanexpress } from "react-icons/si"
 
 export default function CheckoutForm() {
 
-    const { register, handleSubmit, formState: { errors }, watch } = useForm()
+    const { register, handleSubmit, formState: { errors }, watch } = useForm({
+        defaultValues: {
+            paymentmethod: "card"
+        }
+    })
 
     const chekoutPaymetnOption = watch("paymentmethod")
 
@@ -38,9 +44,32 @@ export default function CheckoutForm() {
 
             </div>
             <h3>Datos de pago</h3>
-            <ChekoutPaymetOptions register={register("paymentmethod")} />
+            <ChekoutPaymetOptions register={register("paymentmethod")} current={chekoutPaymetnOption} />
 
+            <h3>
+                Datos de la tarjeta
+            </h3>
 
+            <div className='az-form-controls  my-2 mt-5' >
+                <label>
+                    <span>Numero de la tajeta *</span>
+                    <div className='input-card'>
+                        <input type="text" {...register("email")} />
+                        <div className="az-icon-card-zone">
+                            <span>
+                                <FaCcVisa />
+                            </span>
+                            <span>
+                                <FaCcMastercard />
+                            </span>
+                            <span>
+                                <SiAmericanexpress />
+                            </span>
+
+                        </div>
+                    </div>
+                </label>
+            </div>
 
         </form>
     )
--- components/forms/ChekoutPaymetOptions.jsx
+++ components/forms/ChekoutPaymetOptions.jsx
@@ -1,21 +1,47 @@
 import React from 'react'
+import { SiZelle } from "react-icons/si";
+import { FaPaypal } from "react-icons/fa6";
+import { FaCreditCard } from "react-icons/fa6";
+
 
 export default function ChekoutPaymetOptions({ register, current, option }) {
+
+
+    console.log("el metodo actual es ", current)
+
     return (
+
         <div className=' az-chekout-payment-option mt-4'>
             <p>forma de pago</p>
             <div className='flex flex-wrap w-full '>
                 <label className='az-chekout-payment-radio'>
-                    <input value={"card"} type="radio" {...register} />
-                    <span>Tarjeta</span>
+                    <input value={"card"}  type="radio" checked={current==="card"} {...register} />
+                    <span>
+                        <h6>Tarjeta</h6>
+                        <span className='icon'>
+                            <FaCreditCard/>
+                        </span>
+                    </span>
                 </label>
                 <label className='az-chekout-payment-radio'>
-                    <input value={"paypal"} type="radio" {...register} />
-                    <span>Paypal</span>
+                    <input value={"paypal"} type="radio" checked={current ==="paypal"}  {...register} />
+                    <span>
+                        <h6>
+                            Paypal
+                        </h6>
+                        <span className="icon">
+                            <FaPaypal/>
+                        </span>
+                    </span>
                 </label>
                 <label className='az-chekout-payment-radio'>
-                    <input value={"paypal"} type="radio" {...register} />
-                    <span>Zinli</span>
+                    <input value={"zelle"} type="radio" className='pr-[100px]' checked={current === "zelle"} {...register} />
+                    <span>
+                        <h6>Zelle</h6>
+                        <span className="icon">
+                            <SiZelle/>
+                        </span>
+                    </span>
                 </label>
 
             </div>
